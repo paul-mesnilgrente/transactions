65bf24c8ad8f4b9ab8ef20e9d0777e62feb161f8
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2026-06-24 16:03:13 +0200

    conf: added github workflow to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Repo is served at https://paul-mesnilgrente.github.io/transactions/
+  base: '/transactions/',
   plugins: [react()],
 })
